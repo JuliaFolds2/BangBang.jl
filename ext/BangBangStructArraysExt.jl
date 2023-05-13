6324ab2167a7cf879e8473906243ad7841481cdd
Mason Protter <mason.protter@icloud.com>
2023-05-13 13:18:21 -0600

    Update ext/BangBangStructArraysExt.jl

--- ext/BangBangStructArraysExt.jl
+++ ext/BangBangStructArraysExt.jl
@@ -1,6 +1,11 @@
 module BangBangStructArraysExt
+@static if isdefined(Base, :get_extension)
     using BangBang: BangBang
     using StructArrays: StructArrays
+else
+    using ../BangBang: BangBang
+    using ../StructArrays: StructArrays
+end
     if isdefined(StructArrays, :append!!)
         BangBang.append!!(xs::StructArrays.StructVector, ys) =
             StructArrays.append!!(xs, ys)
