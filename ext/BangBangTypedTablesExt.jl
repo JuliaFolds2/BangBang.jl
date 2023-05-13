478ae5a2b723bee590ae96d93b65d12b6bd13c5b
Mason Protter <mason.protter@icloud.com>
2023-05-13 13:28:24 -0600

    Update BangBangTypedTablesExt.jl

--- ext/BangBangTypedTablesExt.jl
+++ ext/BangBangTypedTablesExt.jl
@@ -4,9 +4,9 @@ module BangBangTypedTablesExt
     using BangBang.NoBang 
     using TypedTables
 else
-    using ../BangBang
-    using ../BangBang.NoBang 
-    using ../TypedTables
+    using ..BangBang
+    using ..BangBang.NoBang 
+    using ..TypedTables
 end
     Base.append!(dest::TypedTables.Table, src::NoBang.SingletonVector{<:NamedTuple}) =
     push!(dest, first(src))
