e9dc8fa87165c76523118d2339ecf33ce2413b7c
Mason Protter <mason.protter@icloud.com>
2023-05-13 13:27:23 -0600

    Update BangBangDataFramesExt.jl

--- ext/BangBangDataFramesExt.jl
+++ ext/BangBangDataFramesExt.jl
@@ -5,10 +5,10 @@ module BangBangDataFramesExt
     using DataFrames
     using DataFrames.Tables: Tables
 else
-    using ../BangBang
-    using ../Base: setindex!
-    using ../DataFrames
-    using ../DataFrames.Tables: Tables
+    using ..BangBang
+    using ..Base: setindex!
+    using ..DataFrames
+    using ..DataFrames.Tables: Tables
 end
     include("dataframes_impl.jl")
     BangBang.push!!(df::DataFrames.DataFrame, row) = df_append_rows!!(df, (row,))
