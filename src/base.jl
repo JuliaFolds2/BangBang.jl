e679bae34a531ad8adc7dc1d896028958be2e532
Tor Erlend Fjelde <tor.github@gmail.com>
2024-01-27 11:02:33 +0000

    Fix for `setindex!!` with array values (#16)
    * fixed type instability in setindex! with array values
    
    * removed TODO comment
    
    * renamed one is_linear_index to test_linear_index_only + return false
    for vector
    
    * fix issue with `possible` for `_setindex!` not handling invalid array values
    
    * move things around and add further description to the code
    
    * fixed comment typo
    
    * fix typos in tests
    
    * drop eltype restrictions
    
    * add a non-<:Real test case
    
    ---------
    
    Co-authored-by: Mason Protter <mason.protter@icloud.com>

--- src/base.jl
+++ src/base.jl
@@ -481,12 +481,49 @@ Base.@propagate_inbounds _setindex!(xs, v, I...) = (setindex!(xs, v, I...); xs)
 
 pure(::typeof(_setindex!)) = NoBang._setindex
 possible(::typeof(_setindex!), ::Union{Tuple,NamedTuple,Empty}, ::Vararg) = false
-possible(::typeof(_setindex!), ::C, ::T, ::Vararg) where {C <: AbstractArray, T} =
-    implements(setindex!, C) && promote_type(eltype(C), T) <: eltype(C)
 possible(::typeof(_setindex!), ::C, ::V, ::K) where {C <: AbstractDict, V, K} =
     implements(setindex!, C) &&
     promote_type(keytype(C), K) <: keytype(C) &&
     promote_type(valtype(C), V) <: valtype(C)
+
+# Default implementation for `_setindex!` with `AbstractArray`.
+# But this will return `false` even in cases such as
+#
+#     setindex!!([1, 2, 3], [4, 5, 6], :)
+#
+# because `promote_type(eltype(C), T) <: eltype(C)` is `false`.
+possible(::typeof(_setindex!), ::C, ::T, ::Vararg) where {C <: AbstractArray, T} =
+    implements(setindex!, C) && promote_type(eltype(C), T) <: eltype(C)
+
+# To address this, we specialize on the case where `T<:AbstractArray`.
+# In addition, we need to support a wide range of indexing behaviors:
+#
+# We also need to ensure that the dimensionality of the index is
+# valid, i.e. that we're not returning `true` in cases such as
+#
+#     setindex!!([1, 2, 3], [4, 5], 1)
+#
+# which should return `false`.
+_index_dimension(::Any) = 0
+_index_dimension(::Colon) = 1
+_index_dimension(::AbstractVector) = 1
+_index_dimension(indices::Tuple) = sum(map(_index_dimension, indices))
+
+function possible(
+    ::typeof(_setindex!),
+    ::C,
+    ::T,
+    indices::Vararg
+    ) where {M, C <: AbstractArray{<:Any}, T <: AbstractArray{<:Any,M}}
+    return implements(setindex!, C) &&
+        promote_type(eltype(C), eltype(T)) <: eltype(C) &&
+        # This will still return `false` for scenarios such as
+        #
+        #     setindex!!([1, 2, 3], [4, 5, 6], :, 1)
+        #
+        # which are in fact valid. However, this cases are rare.
+        (_index_dimension(indices) == M || _index_dimension(indices) == 1)
+end
 
 """
     resize!!(vector::AbstractVector, n::Integer) -> vector′
