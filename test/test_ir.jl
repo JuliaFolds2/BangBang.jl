a0a959d1d2ff04da2d1a771832978ca4f8aee724
Mason Protter <mason.protter@gmail.com>
2025-02-25 16:40:11 +0100

    disable IR tests

--- test/test_ir.jl
+++ test/test_ir.jl
@@ -1,6 +1,9 @@
 module TestIR
 
-using PerformanceTestTools
-PerformanceTestTools.@include("__test_ir.jl")
+# TODO: maybe re-enable these if someone has the motivation to rewrite them to work on
+# modfern julia versions:
+
+#using PerformanceTestTools
+#PerformanceTestTools.@include("__test_ir.jl")
 
 end  # module
