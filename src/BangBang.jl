151cf271be5d385cbb98ac90871b411173e8819e
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2025-02-25 15:44:09 +0100

    Bump Julia compat to 1.10
    Since Julia 1.9 is no longer supported, presumably future versions of BangBang
    would not need to support it.
    Removing 1.9 support allows the Requires dependency to be deleted.

--- src/BangBang.jl
+++ src/BangBang.jl
@@ -49,10 +49,6 @@ using ConstructionBase: constructorof
 using InitialValues
 using LinearAlgebra
 
-@static if !isdefined(Base, :get_extension)
-    using Requires
-end
-
 include("utils.jl")
 
 # Used in NoBang:
@@ -76,28 +72,5 @@ include("macro.jl")
 include("accessors.jl")
 
 using .AccessorsImpl: @set!!, prefermutation
-
-function __init__()
-    @static if !isdefined(Base, :get_extension)
-        @require StaticArrays = "90137ffa-7385-5640-81b9-e52037218182" begin
-            include("../ext/BangBangStaticArraysExt.jl")
-        end
-        @require StructArrays = "09ab397b-f2b6-538f-b94a-2f83cf4a842a" begin
-            include("../ext/BangBangStructArraysExt.jl")
-        end
-        @require Tables = "bd369af6-aec1-5ad0-b16a-f7cc5008161c" begin
-            include("../ext/BangBangTablesExt.jl")
-        end
-        @require TypedTables = "9d95f2ec-7b3d-5a63-8d20-e2491e220bb9" begin
-            include("../ext/BangBangTypedTablesExt.jl")
-        end
-        @require DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0" begin
-            include("../ext/BangBangDataFramesExt.jl")
-        end
-        @require ChainRulesCore = "d360d2e6-b24c-11e9-a2a3-2a2ae2dbcce4" begin
-            include("../ext/BangBangChainRulesCoreExt.jl")
-        end
-    end
-end
 
 end # module
